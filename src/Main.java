6e59ebec2868f202cb50badd94ecd1d50f484428
XXX <ac4tes@gmail.com>
2026-04-01 02:40:28 +0300

    task_5

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,10 @@
 void main() {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
-    var friend = 19;
-    System.out.println ("friend = " + friend);
-    System.out.println ("friend*2 = " + friend * 2);
-    System.out.println ("friend*2/7 = " + friend * 2 / 7);
+    var frog = 3.5;
+    System.out.println ("frog = " + frog);
+    System.out.println ("frog*10 = " + frog * 10);
+    System.out.println ("frog*10/3.5 = " + frog * 10 / 3.5);
+    System.out.println ("frog*10/3.5+4 = 14.0");
+    // сорри))) не получилось записать формулой, потому что оно всё времы прибавляло к десятичным, т.е. получалось 10.4
 }
