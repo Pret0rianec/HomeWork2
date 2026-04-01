4eda5230b74a9d06b776c1f16d1c73cca844e8c4
XXX <ac4tes@gmail.com>
2026-04-01 21:09:22 +0300

    task_8.2

--- src/Main.java
+++ src/Main.java
@@ -6,4 +6,8 @@ void main() {
     var allHours = 640;
     var shiftEmployee = 8;
     System.out.println("Всего работников в компании — " + (allHours/shiftEmployee) + " человек");
+    var employees = allHours/shiftEmployee;
+    var bigEmployees = employees + 94;
+    var ultraAllHours = bigEmployees * 8;
+    System.out.println("Если в компании работает " + bigEmployees + " человек(а), то всего " + ultraAllHours + " час(а/ов) работы может быть поделено между сотрудниками");
     }
